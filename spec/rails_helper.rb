aa4823ab1c56a2f3c6bd6b97192eb47c67a52a3d
ken-sayama <sayamaken0402@gmail.com>
2018-10-23 11:09:26 +0900

    settings database_rewinder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,13 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseRewinder.clean_all
+  end
+
+  config.after(:each) do
+    DatabaseRewinder.clean
+  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
