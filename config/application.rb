ae19905cff193165ed1470a70779476e5c874680
ken-sayama <sayamaken0402@gmail.com>
2018-10-23 11:37:31 +0900

    bug fix

--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,12 @@ module RspecPlactice
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    g.test_framework :rspec,
-       fixtures: false,
-       view_specs: false,
-       helper_specs: false,
-       routing_specs: false
+    config.generators do |g|
+      g.test_framework :rspec,
+         fixtures: false,
+         view_specs: false,
+         helper_specs: false,
+         routing_specs: false
+    end
   end
 end
